a83fe6508f0173fc21be1ef82ed394bd4b68147e
Ionică Bizău <bizauionica@gmail.com>
2019-11-24 20:10:17 -0300

    :arrow_up: 2.3.0 :tada:

--- example/index.js
+++ example/index.js
@@ -1,14 +1,14 @@
 // Dependencies
-var Streamp = require("../lib");
+const Streamp = require("../lib")
 
 // Constants
-const PATH = __dirname + "/long/path/to/foo.txt";
+const PATH = __dirname + "/long/path/to/foo.txt"
 
 // Create writable stream
-var bar = new Streamp.writable(PATH);
-bar.write(new Date().toString() + "\n");
-bar.end();
+const bar = new Streamp.writable(PATH)
+bar.write(new Date().toString() + "\n")
+bar.end()
 
 // Create readable stream
-var foo = new Streamp.readable(PATH);
-foo.pipe(process.stdout);
+const foo = new Streamp.readable(PATH)
+foo.pipe(process.stdout)
